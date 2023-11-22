fe5b9b4f1e6b4a5e2157ad28353e4c25b6b5dd4a
Nelson Gutierrez <nelson.gutierrez.vega@gmail.com>
2023-11-21 23:27:18 -0600

    add svg

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -5,7 +5,16 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-    <h1 class="text-2xl md:text-4xl text-center mt-8">Color Game</h1>
+    <header class="flex flex-col items-center justify-center mt-8">
+      <h1 class="text-2xl md:text-4xl text-center  ">Colors</h1>
+      <svg width="100" height="20" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="10" cy="10" r="5" fill="#00457C" />
+        <circle cx="30" cy="10" r="5" fill="#008000" />
+        <circle cx="50" cy="10" r="5" fill="#FFD700" />
+        <circle cx="70" cy="10" r="5" fill="#7C0A02" />
+        <circle cx="90" cy="10" r="5" fill="#FFA500" />
+      </svg>
+    </header>
   `,
 })
 export class HeaderComponent {}
